48ea048181690e11955823b9fcfe47d4b492beb4
RexRegum <56251425+RexRegum@users.noreply.github.com>
2021-03-08 11:08:47 +0100

    player stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Idle Animation
+    public GameObject IdleSprite;
+
+    // Attack sprites
+    public GameObject AttackReady;
+    public GameObject AttackHit;
+    public GameObject BlockSprite;
+
+    public bool blocking = false;
+    
+
+    void Update()
+    {
+        // Block when press button
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            blocking = true;
+        } else
+        {
+            blocking = false;
+        }
+    }
+
+    public IEnumerator Attack1()
+    {
+        AttackReady.SetActive(true);
+        IdleSprite.SetActive(false);
+        yield return new WaitForSecondsRealtime(1);
+        AttackReady.SetActive(false);
+        AttackHit.SetActive(true);
+        // If enemy is not blocking, deal damage (epicly)
+        yield return new WaitForSecondsRealtime(0.5f);
+        AttackHit.SetActive(false);
+        IdleSprite.SetActive(true);
+    }
+}
